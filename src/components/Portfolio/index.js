4fb918b871ae8589134ce20198c73a593bc0d455
Tech-outlook <techoutlook9@gmail.com>
2022-01-26 20:25:06 +0530

    init

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,35 +1,9 @@
-import { useState, useEffect } from "react";
-import database from "../../firebase";
 import "./style.css";
 const Portfolio = () => {
-  const [getData, setGetData] = useState({});
-
-  useEffect(() => {
-    database.child("apidata").on("value", (details) => {
-      setGetData(details.val());
-    });
-  }, []);
-
-  console.log(getData);
 
   return (
     <div className="Portfolio">
-      {getData &&
-        Object.keys(getData).map((key) => (
-          <div className="post" key={getData[key].id}>
-            <img src={getData[key].Source_code_link} alt={getData[key].title} className="post-image" />
-            <div>
-              <h4>{getData[key].title}</h4>
-              <a
-                href={getData[key].Source_code_link}
-                target={"_blank"}
-                rel="noopener noreferrer"
-              >
-                <button className="btn btn-success">Source Code</button>
-              </a>
-            </div>
-          </div>
-        ))}
+      
     </div>
   );
 };
