f77c17e00946ba874ea3525c735d494e9bbf220d
Tech-outlook <techoutlook9@gmail.com>
2022-01-26 17:43:22 +0530

    init

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import database from "../../firebase";
 import "./style.css";
+import images from "./Assets/customized bootstrap navbar JPEG.jpg";
 
 const Portfolio = () => {
   const [getData, setGetData] = useState({});
@@ -18,10 +19,17 @@ const Portfolio = () => {
       {getData &&
         Object.keys(getData).map((key) => (
           <div className="post" key={getData[key].id}>
-            <img
-              src={getData[key].Image_source_url}
-              alt={getData[key].title}
-            />
+            <img src={images} alt={getData[key].title} className="post-image" />
+            <div>
+              <h4>{getData[key].title}</h4>
+              <a
+                href={getData[key].Source_code_link}
+                target={"_blank"}
+                rel="noopener noreferrer"
+              >
+                <button className="btn btn-success">Source Code</button>
+              </a>
+            </div>
           </div>
         ))}
     </div>
